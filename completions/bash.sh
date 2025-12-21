2d9bd35e4146e9192616883ab881e02258297391
Justin Trahan <justin@trahan.dev>
2025-12-21 13:08:46 -0600

    d

--- completions/bash.sh
+++ completions/bash.sh
@@ -1,14 +1,34 @@
-_dnsctl_complete() {
-	local cur prev opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
+# Bash completion for dnsctl
+#
+# Installation (recommended):
+#   dnsctl completion bash > /etc/bash_completion.d/dnsctl
+#
+# Or for current user only:
+#   dnsctl completion bash > ~/.bash_completion
+#
+# Reload shell:
+#   source ~/.bashrc
+#
+# Temporary (current shell only):
+#   source <(dnsctl completion bash)
 
-	opts="--timeout --workers --dry-run --help shellcompletion"
+_dnsctl() {
+  local cur prev
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-	if [[ ${cur} == -* ]]; then
-		COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-		return 0
-	fi
+  case "${COMP_WORDS[1]}" in
+    clean-zones)
+      COMPREPLY=( $(compgen -W "--file --timeout --workers --dry-run" -- "$cur") )
+      ;;
+    completion)
+      COMPREPLY=( $(compgen -W "bash zsh" -- "$cur") )
+      ;;
+    *)
+      COMPREPLY=( $(compgen -W "clean-zones completion" -- "$cur") )
+      ;;
+  esac
 }
-complete -F _dnsctl_complete dnsctl
+
+complete -F _dnsctl dnsctl
--- completions/zsh.sh
+++ completions/zsh.sh
@@ -1,8 +1,44 @@
+# Zsh completion for dnsctl
+#
+# Installation (recommended):
+#   mkdir -p ~/.zsh/completions
+#   dnsctl completion zsh > ~/.zsh/completions/_dnsctl
+#
+# Ensure the following is in your ~/.zshrc:
+#   fpath+=~/.zsh/completions
+#   autoload -Uz compinit
+#   compinit
+#
+# Reload shell:
+#   exec zsh
+#
+# Temporary (current shell only):
+#   source <(dnsctl completion zsh)
+
 #compdef dnsctl
 
-_arguments \
-  '--timeout[Ping timeout]:duration' \
-  '--workers[Number of parallel workers]:int' \
-  '--dry-run[Do not modify output]' \
-  'shellcompletion[Generate shell completion]:shell:(bash zsh)' \
-  '*:file:_files'
+_dnsctl() {
+  _arguments \
+    '1:command:(clean-zones completion)' \
+    '*::arg:->args'
+
+  case $state in
+    args)
+      case $words[2] in
+        clean-zones)
+          _arguments \
+            '--file[Path to YAML file]:file:_files' \
+            '--timeout[Ping timeout]' \
+            '--workers[Number of workers]' \
+            '--dry-run[Do not write output]'
+          ;;
+        completion)
+          _arguments \
+            '1:shell:(bash zsh)'
+          ;;
+      esac
+    ;;
+  esac
+}
+
+compdef _dnsctl dnsctl
